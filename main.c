a36845c18ba48a53805db8f963b19346169f651a
espi3000 <60798059+espi3000@users.noreply.github.com>
2021-04-19 15:15:51 +0200

    Added project files

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * project.c
+ *
+ * Created: 19.04.2021 15:09:55
+ * Author : espen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
